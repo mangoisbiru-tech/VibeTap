86ddb5decb2d55a030f2088de51cde48dc0cc198
mangoisbiru-tech <mangoisbiru@gmail.com>
2026-04-27 15:33:16 +0800

    Deployment Debug Test

---
+++ app/debug/page.tsx
@@ -0,0 +1,7 @@
+export default function DebugPage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Debug Page - Version 2.0.1</h1>
+    </div>
+  );
+}
